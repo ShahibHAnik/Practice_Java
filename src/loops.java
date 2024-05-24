1a820d6c8b605df7c4131608200fe4f6a73e2ebd
ShahibHAnik <mdanik@asa.edu>
2024-05-24 19:58:56 -0400

    loops practice

---
+++ src/loops.java
@@ -0,0 +1,31 @@
+public class loops {
+    //while loop
+    //for loop
+    //do while loop
+
+
+    //for loops formula
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
+            System.out.println(i);
+        }
+
+
+
+        //while loops
+        int i =1;
+        while(i<=8){
+            System.out.println(i);
+            i++;  //make sure put the i++ for while loops otherwise it gonna crush you pc
+
+        }
+
+        //do while loops
+        int number =1;
+        do {
+            System.out.println(number);
+            number++;
+        }
+        while(number <= 7);
+    }
+}
